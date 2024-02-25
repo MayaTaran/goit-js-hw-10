f399981e9e5f1b818f61127f9b78d12292092461
MayaTaran <145223827+MayaTaran@users.noreply.github.com>
2024-02-25 19:13:13 +0200

    first version (only timer files)

---
+++ src/js/1-timer.js
@@ -0,0 +1,97 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const myInput = document.querySelector("input#datetime-picker");
+const getButton = document.querySelector(".timer-button");
+getButton.disabled = true;
+
+let userSelectedDate; 
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+
+    let currentDate = Date.now();
+    if (currentDate > userSelectedDate.getTime()) {
+      iziToast.show({
+    title: '',
+          message: 'Please choose a date in the future',
+          messageColor: '#FFF',
+          position: 'topRight',
+          color: '#B51B1B'
+});
+    } else {
+      getButton.disabled = false;
+    }
+  },
+};
+
+const fp = flatpickr(myInput, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value){
+    return String(value).padStart(2, "0");
+};
+function updateTimer() {
+    const ms = userSelectedDate.getTime() - Date.now();
+
+   if (ms <= 0) {
+    clearInterval(timerInterval);
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+    getButton.disabled = true; 
+  } else {
+    const timeObject = convertMs(ms);
+
+    document.getElementById("days").innerText = addLeadingZero(timeObject.days);
+    document.getElementById("hours").innerText = addLeadingZero(timeObject.hours);
+    document.getElementById("minutes").innerText = addLeadingZero(timeObject.minutes);
+    document.getElementById("seconds").innerText = addLeadingZero(timeObject.seconds);
+  }
+}
+
+getButton.addEventListener("click", function() {
+   updateTimer();
+getButton.disabled = true;
+  myInput.disabled = true;
+   const timerInterval = setInterval(updateTimer, 1000);
+
+});
